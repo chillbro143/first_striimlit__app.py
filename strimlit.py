32f1b6ca18c5813f592be548ccc1e146ceb2e06c
chillbro143 <83223813+chillbro143@users.noreply.github.com>
2023-08-04 12:02:52 +0530

    Update strimlit.py

--- strimlit.py
+++ strimlit.py
@@ -2,3 +2,5 @@ import streamlit
 streamlit.title("My parents new healthy diner")
 streamlit.header('breakfast menu')
 streamlit.text('omega 3 and Blueberry ooatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
