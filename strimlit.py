a1c598ef6e9718d576fd414c62e07045acaca0bd
chillbro143 <83223813+chillbro143@users.noreply.github.com>
2023-08-07 16:12:30 +0530

    Update strimlit.py

--- strimlit.py
+++ strimlit.py
@@ -15,7 +15,7 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
 
 # taking json version of file and normalising it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
