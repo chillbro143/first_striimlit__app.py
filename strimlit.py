8d47c48ec4d2e60dc44e39eb5bd1fc39e2018a50
chillbro143 <83223813+chillbro143@users.noreply.github.com>
2023-08-07 16:16:47 +0530

    Update strimlit.py

--- strimlit.py
+++ strimlit.py
@@ -16,7 +16,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Apple')
 streamlit.write('The user entered ', fruit_choice)
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/+fruit_choice")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 
 # taking json version of file and normalising it
