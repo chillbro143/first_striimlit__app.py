7ee9d3d75a31723931355148b156ec9e3804bef9
chillbro143 <83223813+chillbro143@users.noreply.github.com>
2023-08-08 11:39:24 +0530

    Update strimlit.py

--- strimlit.py
+++ strimlit.py
@@ -36,60 +36,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
- 
-
-#create the repeatable code block (called a function)
-def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return fruityvice_normalized
 
  
 
-#new section to display fruitvice api reposnse
-streamlit.header('Fruityvice Fruit Advice!')
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("please select a fruit to get information.")
-  else:
-      back_from_functon = get_fruityvice_data(fruit_choice)
-      streamlit.dataframe(back_from_functon )
 
- 
-
-except URLError as e:
-   streamlit.error()
 
  
 
  
 
-streamlit.header("View Our Fruit list- Add your Faviurotes!:")
-def get_fruit_load_list():
-with my_cnx.cursor() as my_cur:
-my_cur.execute("SELECT* from fruit_load_list")
-return my_cur.fetchall()
-
- 
-
-#   #Add a button to load the fruit
-# if streamlit.button('get fruit load list'):
-#   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#   my_data_rows = get_fruit_load_list()
-#   my_cnx.close()
-#   streamlit.dataframe(my_data_rows)
-
- 
-
- 
-
-# #Allow the end user to add fruit to the list
-# # def insert_row_snowflake(new_fruit):
-# #     with my_cnx.cursor() as my_cur:
-# #       my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-# #       return "Thanks for adding "+new_fruit
-# def insert_row_snowflake(new_fruit):
-#   with my_cnx.cursor() as my_cur:
-#     my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
-#     return "thanks for adding " + new_fruit
