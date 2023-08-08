aa490417b080129799a5d9b150097e78a6637d0a
chillbro143 <83223813+chillbro143@users.noreply.github.com>
2023-08-08 12:24:30 +0530

    Update strimlit.py

--- strimlit.py
+++ strimlit.py
@@ -43,25 +43,13 @@ def get_fruityvice_data(this_fruit_choice):
   return fruityvice_normalized
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-# my_cur = my_cnx.cursor()
-# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-# my_data_row = my_cur.fetchone()
-# streamlit.text("Hello from Snowflake:")
-# streamlit.text(my_data_row)
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
-streamlit.header('Fruityvice Fruit Advice!')
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("please select a fruit to get information.")
-  else:
-      back_from_functon = get_fruityvice_data(fruit_choice)
-      streamlit.dataframe(back_from_functon )
 
- 
-
-except URLError as e:
-   streamlit.error()
 
  
 
